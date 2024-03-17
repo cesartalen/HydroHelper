9a8693b6f44b811aac1ad26c25927bf2038550e6
cesartalen <cesartalense@gmail.com>
2024-03-18 00:49:39 +0100

    update waterlog for today

---
+++ actions/update-water.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import { db } from '@/lib/db'
+import { todayWaterlog } from './today-waterlog'
+
+export const updateWater = async (userId: any, amount: number) => {
+  const waterLog = await todayWaterlog(userId)
+  const totalWater = waterLog.amount + amount
+
+  await db.waterLog.update({
+    where: { id: waterLog.id },
+    data: { amount: totalWater },
+  })
+}
