6443e42b734ec093f10ef134e1928a85b0ac1bc4
cesartalen <cesartalense@gmail.com>
2024-03-21 10:39:46 +0100

    Loading until page content is loaded

--- app/track/page.tsx
+++ app/track/page.tsx
@@ -12,6 +12,7 @@ const TrackPage = () => {
   const [goalPercentage, setGoalPercentage] = useState(0)
   const [goal, setGoal] = useState(0)
   const [waterPreset, setWaterPreset] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   if(status === "unauthenticated") {
     redirect('/')
@@ -30,6 +31,8 @@ const TrackPage = () => {
       setGoal(preferences.dailyGoal)
       setWaterPreset(preferences.waterPreset)
     }
+
+    setLoading(false)
   }
 
   const getWater = async () => {
@@ -48,15 +51,23 @@ const TrackPage = () => {
     <>
       <main className='flex break-all flex-col md:flex-row'>
         <div className='rounded-xl border px-6 py-4 mx-6 my-6 md:w-3/4'>
-          <p>Welcome {session?.user?.name} : {session?.user?.id}</p>
-          <p>{water}</p>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div className="bg-cyan-500 h-2.5 rounded-full" style={{width: `${Math.min((water * 100) / goal, 100)}%`}}></div>
+          {loading ? (
+            <>
+              <p>Loading...</p>
+            </>
+          ): (
+            <>
+              <p>Welcome {session?.user?.name} : {session?.user?.id}</p>
+              <p>{water}</p>
+              <div className="w-full bg-gray-200 rounded-full h-2.5">
+                <div className="bg-cyan-500 h-2.5 rounded-full" style={{width: `${Math.min((water * 100) / goal, 100)}%`}}></div>
+              </div>
+              <button onClick={() => handleWaterClick(waterPreset)}>Update Water</button>
+              <br/>
+              <button onClick={() => signOut()}>Logout</button>
+            </>
+            )}
           </div>
-          <button onClick={() => handleWaterClick(waterPreset)}>Update Water</button>
-          <br/>
-          <button onClick={() => signOut()}>Logout</button>
-        </div>
         <div className='rounded-xl border px-6 py-4 mx-6 my-6 md:w-1/4 '>
         </div>
       </main>
