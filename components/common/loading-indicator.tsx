f150fa5b9c60cb0dad6f01d6ef029dc7482727eb
cesartalen <cesartalense@gmail.com>
2024-03-22 13:14:16 +0100

    spinner when loading

--- components/common/loading-indicator.tsx
+++ components/common/loading-indicator.tsx
@@ -1,7 +1,10 @@
 export const LoadingIndicator = () => {
   return(
     <>
-      <p>Loading...</p>
+      <div>
+          <p>Loading...</p>
+          <svg className="animate-spin h-4 w-4 rounded-full bg-transparent border-2 border-transparent border-opacity-75 border-t-cyan-700 border-r-cyan-700 border-l-cyan-700" viewBox="0 0 24 24"></svg>
+      </div>
     </>
   )
 }
