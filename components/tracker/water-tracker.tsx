7c6d195c212a75b528083b22744a6aab1344e5d4
cesartalen <cesartalense@gmail.com>
2024-03-23 21:45:43 +0100

    improved responsibility

--- components/tracker/water-tracker.tsx
+++ components/tracker/water-tracker.tsx
@@ -26,7 +26,7 @@ export const WaterTracker = ({name, userId, goal, waterPreset} : {name : any, us
   }, [userId])
 
   return (
-    <div className='rounded-xl border px-6 py-4 mx-6 md:w-2/3 xl:w-3/4'>
+    <div className='rounded-xl border px-6 py-4 mx-6 md:w-2/3 xl:w-3/4 mb-6 md:mb-0'>
       {loading ? (
         <LoadingIndicator/>
       ) : (
