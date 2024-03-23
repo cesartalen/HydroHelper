8e1ca471cfeff8aa60a55f90579470d25111ec8b
cesartalen <cesartalense@gmail.com>
2024-03-23 20:50:31 +0100

    show stats of last 7 days in stats panel

--- components/tracker/water-stats-panel.tsx
+++ components/tracker/water-stats-panel.tsx
@@ -1,5 +1,29 @@
-export const WaterStatsPanel = () => {
+"use client"
+
+import { getWeekWaterLogs } from '@/actions/get-week-waterlogs'
+import { useEffect, useState } from 'react'
+
+export const WaterStatsPanel = ({userId} : {userId: any}) => {
+  const [waterLogs, setWaterLogs] = useState<any[]>([])
+
+  const fetchWaterLogs = async () => {
+    const logs = await getWeekWaterLogs(userId)
+    setWaterLogs(logs)  
+  }
+
+  useEffect(() => {
+    fetchWaterLogs()
+  }, [])
+  
   return(
-    <div className='rounded-xl border px-6 py-4 mx-6 md:w-1/4'></div>
+    <div className='rounded-xl border px-6 py-4 mx-6 md:w-1/4'>
+      <ul>
+        {waterLogs.map((val, key) => (
+          <li key={key}>
+            <a>{val.date.getDay()}: {val.amount}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
