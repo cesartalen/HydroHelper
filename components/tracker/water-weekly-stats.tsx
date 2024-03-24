f458e9424389eff55d3fcdca8d322e9093324167
cesartalen <cesartalense@gmail.com>
2024-03-24 15:51:18 +0100

    show stats on previous days even if there is nothing tracked

--- components/tracker/water-weekly-stats.tsx
+++ components/tracker/water-weekly-stats.tsx
@@ -4,6 +4,18 @@ import { getWeekWaterLogs } from '@/actions/get-week-waterlogs'
 import { addDays, format } from 'date-fns'
 import { useEffect, useState } from 'react'
 
+const createWeekDates = () => {
+  const today = new Date()
+  today.setHours(0,0,0,0)
+  const prevWeekDates = []
+
+  for (let i = 0; i < 7; i++) {
+    prevWeekDates.push(addDays(today, -i))
+  }
+
+  return prevWeekDates
+}
+
 export const WaterWeeklyStats = ({userId} : {userId: any}) => {
   const [waterLogs, setWaterLogs] = useState<any[]>([])
 
@@ -16,27 +28,20 @@ export const WaterWeeklyStats = ({userId} : {userId: any}) => {
     fetchWaterLogs()
   }, [])
 
-  const createWeekDates = () => {
-    const today = new Date()
-    today.setHours(0,0,0,0)
-    const prevWeekDates = []
-  
-    for (let i = 0; i < 7; i++) {
-      prevWeekDates.push(addDays(today, -i))
-    }
-  
-    return prevWeekDates
-  }
+  const prevWeekDays = createWeekDates()
 
   return(
     <ul>
     <p>Average this week:</p> 
     {<p className='font-bold text-cyan-700'>{Math.floor(waterLogs.reduce((sum, log) => sum + log.amount, 0) / waterLogs.length)} ml</p>}
-    {waterLogs.map((val, key) => (
-      <li key={key}>
-        <a>{format(val.date, 'E')}: {val.amount}</a>
-      </li>
-    ))}
+    {prevWeekDays.map((date) => {
+    const log = waterLogs.find((log) => format(log.date, 'yyyy-MM-dd') === format(date, 'yyyy-MM-dd'))
+      return(
+        <li key={date.getTime()}>
+          <a>{format(date, 'E')}: {log ? log.amount : 0}</a>
+        </li>
+      )
+  })}
   </ul>
   )
 }
