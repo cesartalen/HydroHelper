16fe95cddbfc64385751c256f77b9282030fabd2
cesartalen <cesartalense@gmail.com>
2024-03-25 13:24:17 +0100

    created removeWater function

---
+++ actions/remove-water.ts
@@ -0,0 +1,18 @@
+"use server"
+
+import { db } from '@/lib/db'
+import { todayWaterlog } from './today-waterlog'
+
+export const removeWater = async (userId: any, amount: number) => {
+  const waterLog = await todayWaterlog(userId)
+
+  if ((waterLog.amount - amount) >= 0 ) {
+    const totalWater = waterLog.amount - amount
+    await db.waterLog.update({
+      where: { id: waterLog.id },
+      data: { amount: totalWater },
+    })
+  }
+
+  //TODO notify user
+}
