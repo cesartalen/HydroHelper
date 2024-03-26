544fa2b66a0a1c0e430e384477764b398c09b79a
cesartalen <cesartalense@gmail.com>
2024-03-26 15:27:36 +0100

    added updatepreset to settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,6 +2,7 @@
 
 import { getPreferences } from '@/actions/preferences/get-preferences'
 import { UpdateGoal } from '@/components/settings/update-goal'
+import { UpdatePreset } from '@/components/settings/update-preset'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 import { useEffect, useState } from 'react'
@@ -9,11 +10,13 @@ import { useEffect, useState } from 'react'
 const SettingsPage = () => {
   const { data: session, status } = useSession()
   const [goal, setGoal] = useState(0)
+  const [preset, setPreset] = useState(0)
 
   const updatePreferences = async () => {
     const preferences = await getPreferences(session?.user?.id)
 
     setGoal(preferences.dailyGoal)
+    setPreset(preferences.waterPreset)
   }
 
   if(status === "unauthenticated") {
@@ -28,6 +31,7 @@ const SettingsPage = () => {
     <>
       <div className='rounded-xl border px-6 py-4 mx-6  mb-6 md:mb-0'>
         <UpdateGoal userId={session?.user?.id} goal={goal} goalUpdated={updatePreferences}/>
+        <UpdatePreset userId={session?.user?.id} preset={preset} presetUpdated={updatePreferences}/>
       </div>
     </>
   )
