9936f0b62605fa88c32252fdc0475119607a2e24
cesartalen <cesartalense@gmail.com>
2024-03-26 16:20:28 +0100

    loading indicator for settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { getPreferences } from '@/actions/preferences/get-preferences'
+import { LoadingIndicator } from '@/components/common/loading-indicator'
 import { UpdateGoal } from '@/components/settings/update-goal'
 import { UpdatePreset } from '@/components/settings/update-preset'
 import { useSession } from 'next-auth/react'
@@ -9,6 +10,7 @@ import { useEffect, useState } from 'react'
 
 const SettingsPage = () => {
   const { data: session, status } = useSession()
+  const [loading, setLoading] = useState(true)
   const [goal, setGoal] = useState(0)
   const [preset, setPreset] = useState(0)
 
@@ -17,6 +19,8 @@ const SettingsPage = () => {
 
     setGoal(preferences.dailyGoal)
     setPreset(preferences.waterPreset)
+
+    setLoading(false)
   }
 
   if(status === "unauthenticated") {
@@ -30,8 +34,15 @@ const SettingsPage = () => {
   return (
     <>
       <div className='rounded-xl border px-6 py-4 mx-6  mb-6 md:mb-0'>
-        <UpdateGoal userId={session?.user?.id} goal={goal} goalUpdated={updatePreferences}/>
-        <UpdatePreset userId={session?.user?.id} preset={preset} presetUpdated={updatePreferences}/>
+        {loading ? (
+          <LoadingIndicator/>
+        ) : (
+        <>
+          <UpdateGoal userId={session?.user?.id} goal={goal} goalUpdated={updatePreferences}/>
+          <UpdatePreset userId={session?.user?.id} preset={preset} presetUpdated={updatePreferences}/>
+        </>
+        )
+      }
       </div>
     </>
   )
