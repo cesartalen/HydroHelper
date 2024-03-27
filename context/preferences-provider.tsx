40d1b8c947920d4967135c3e9080c4fbeca34710
cesartalen <cesartalense@gmail.com>
2024-03-27 11:31:59 +0100

    preferences provider

---
+++ context/preferences-provider.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { createContext, useState } from 'react'
+
+
+interface PreferencesContextType {
+  goal: any
+  setGoal: React.Dispatch<any>
+  preset: any
+  setPreset: React.Dispatch<any>
+}
+
+export const PreferencesContext = createContext<PreferencesContextType>({
+  goal: undefined,
+  setGoal: () => {},
+  preset: undefined,
+  setPreset: () => {}
+})
+
+export const PreferencesProvider = ({ children } : {
+  children: React.ReactNode
+}) => {
+  const [goal, setGoal] = useState()
+  const [preset, setPreset] = useState()
+
+  return(
+    <PreferencesContext.Provider value={{goal, setGoal, preset, setPreset}}>
+      {children}
+    </PreferencesContext.Provider>
+  )
+}
